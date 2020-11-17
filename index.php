416091130b29e671e64e740d261d9c08c086024f
someone <someone@someplace.com>
2020-11-17 15:15:40 -0500

    version1

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 
 <!doctype html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<title>Login</title>
-    <link rel="stylesheet" type="text/css" href="cp/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="cp/font/font-awesome/css/font-awesome.min.css">
-    <script type="text/javascript" src="cp/js/jquery-3.2.1.min.js"></script>
-</head>
+    <head>
+        <meta charset="utf-8">
+        <title>Login</title>
+        <link rel="stylesheet" type="text/css" href="cp/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="cp/font/font-awesome/css/font-awesome.min.css">
+        <script type="text/javascript" src="cp/js/jquery-3.2.1.min.js"></script>
+    </head>
 
-<body>
-	<?php require "cp/pLogin.php" ?>
-    <?php //prueba ?>
-</body>
+    <body>
+        <?php require "cp/pLogin.php" ?>
+        <?php //prueba1 ?>
+        <?php //prueba2 ?>
+    </body>
 </html>
